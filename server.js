d6391f80a9b8d4cd5e09882a9e28c4fb0308b69c
karan2782 <karantanwar5005@gmail.com>
2026-02-21 20:30:40 +0530

    added cors

--- server.js
+++ server.js
@@ -9,7 +9,14 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000', 
+    'http://127.0.0.1:3000',
+    'https://ecommerce-site-product.netlify.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // MongoDB Connection
